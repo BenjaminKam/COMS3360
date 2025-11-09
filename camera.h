dbb7bdaebc0c7c458af692850b1b2881fe30c67a
BenjaminKam <kambenjamin1k@gmail.com>
2025-11-09 14:00:40 -0600

    Adding gamma transform from linear to provide camera with better context data

--- camera.h
+++ camera.h
@@ -103,10 +103,10 @@ private:
             return color(0, 0, 0);
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec))
+        if (world.hit(r, interval(0.001, infinity), rec))
         {
-            vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+            vec3 direction = rec.normal + random_unit_vector();
+            return 0.1 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
