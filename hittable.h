dc34978a77ddee94b88c192441617e9521742eb6
BenjaminKam <kambenjamin1k@gmail.com>
2025-12-06 13:03:42 -0600

    texture mapping using stb_image

--- hittable.h
+++ hittable.h
@@ -12,6 +12,8 @@ public:
     vec3 normal;
     shared_ptr<material> mat;
     double t;
+    double u;
+    double v;
 
     bool front_face;
 
--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include "helper.h"
 
+#include "texture.h"
 #include "bvh.h"
 #include "camera.h"
 #include "hittable.h"
@@ -8,12 +9,12 @@
 #include "material.h"
 #include "triangle.h"
 
-int main()
+void scene_1()
 {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
 
     for (int a = -11; a < 11; a++)
     {
@@ -79,4 +80,71 @@ int main()
     cam.focus_dist = 10.0;
 
     cam.render(world);
+}
+
+void scene_2()
+{
+    hittable_list world;
+
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
+
+    world.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
+    world.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
+void scene_3()
+{
+    auto earth_texture = make_shared<image_texture>("image1.png");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(0, 0, 12);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(globe));
+}
+
+int main()
+{
+    switch (3)
+    {
+    case 1:
+        scene_1();
+        break;
+
+    case 2:
+        scene_2();
+        break;
+
+    case 3:
+        scene_3();
+        break;
+    }
 }
